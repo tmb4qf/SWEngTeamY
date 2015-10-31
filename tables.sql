c4fb9be9cec3f1beaafd4ba7739769d697aa54fa
ampwd6 <ampwd6@mail.missouri.edu>
2015-10-31 14:18:13 -0500

    added tables

--- tables.sql
+++ tables.sql
@@ -1,74 +1,99 @@
-		CREATE TABLE address(
-			addrID serial PRIMARY KEY,
-			street varchar(50), 
-			city varchar(25),
-			state varchar(2),
-			country varchar(3), 
-			zipcode varchar(5)
-		);
-
-		CREATE TABLE person(
-			id varchar(8) PRIMARY KEY,
-			fname varchar(20),
-			lname varchar(25)
-			pawprint varchar(6),
-			phone_number varchar(10),
-			title varchar(20),
-			FOREIGN KEY(addrID) references address
-		);
-
-		CREATE TABLE organization(
-			orgID serial PRIMARY KEY,
-			name varchar (20)
-		);
-
-		CREATE TABLE applicant(
-			FOREIGN KEY(id) REFERENCES person,
-			FOREIGN KEY(organizationID) references organization(orgID),
-			isStudentWorker boolean,
-			PRIMARY KEY(id)
-		);
-
-		CREATE TABLE accessType(
-			accessID serial PRIMARY KEY,
-			type varchar(15)
-		);
-		
-		CREATE TABLE applicationProcessor(
-			FOREIGN KEY(id) REFERENCES person,
-			jobTitle varchar(15),
-			FOREIGN KEY(access_type_ID) REFERENCES accessType(id),
-			PRIMARY KEY(id)
-		);
-
-		CREATE TABLE ferpaScores(
-			score varchar(3),
-			FOREIGN KEY(id) REFERENCES person,
-			PRIMARY KEY(id)
-		);
-
-
-
-CREATE TABLE application_types(
-	
+CREATE TABLE address(
+	addrID serial PRIMARY KEY,
+	street varchar(50), 
+	city varchar(25),
+	state varchar(2),
+	country varchar(3), 
+	zipcode varchar(5)
 );
 
-CREATE TABLE (
+CREATE TABLE person(
+	id varchar(8) PRIMARY KEY,
+	fname varchar(20),
+	lname varchar(25)
+	pawprint varchar(6),
+	phone_number varchar(10),
+	title varchar(20),
+	FOREIGN KEY(addrID) references address
+);
+
+CREATE TABLE organization(
+	orgID serial PRIMARY KEY,
+	name varchar (20)
+);
+
+CREATE TABLE applicant(
+	FOREIGN KEY(id) REFERENCES person,
+	FOREIGN KEY(organizationID) references organization(orgID),
+	isStudentWorker boolean,
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE accessType(
+	accessID serial PRIMARY KEY,
+	type varchar(15)
+);
+
+CREATE TABLE applicationProcessor(
+	FOREIGN KEY(id) REFERENCES person,
+	jobTitle varchar(15),
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE ferpaScores(
+	FOREIGN KEY(id) REFERENCES person,
+	score varchar(2),
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE careerTypes(
+	typeID serial PRIMARY KEY, 
+	department varchar(7)
+);
+
+CREATE TABLE applicationTypes(
+	typeID serial PRIMARY KEY,
+	type varchar(10)
+);
+
+CREATE TABLE application(
+	appID serial PRIMARY KEY,
+	FOREIGN KEY(id) REFERENCES person,
+	FOREIGN KEY(access_type) REFERENCES applicationTypes
+);
+
+CREATE TABLE requestedCareerTypes(
+	FOREIGN KEY(appID) REFERENCES application,
+	FOREIGN KEY(id) REFERENCES person,
+	FOREIGN KEY(typeID) REFERENCES careerTypes,
+	PRIMARY KEY(appID, id, typeID)
+);
+
+CREATE TABLE admissionsTestRequest(
 
 );
 
-CREATE TABLE 
+CREATE TABLE admissionsTest(
 
 );
 
-CREATE TABLE (
-
+CREATE TABLE roleType(
+	typeID serial PRIMARY KEY, 
+	name varchar(20)
 );
 
-CREATE TABLE (
-
+CREATE TABLE roles(
+	roleID serial PRIMARY KEY,
+	FOREIGN KEY(roleType) REFERENCES roleType(typeID),
+	roleDesc varchar(150),
+	isViewable boolean, 
+	isUpdateable boolean
 );
 
-CREATE TABLE 
-
+CREATE TABLE roleAccessRequest(
+	roleAccessID serial PRIMARY KEY,
+	FOREIGN KEY(appID) REFERENCES application,
+	FOREIGN KEY(roleID) REFERENCES roles,
+	isViewRequest
+	isUpdateRequest
 );
