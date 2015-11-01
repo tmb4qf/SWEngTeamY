d48682523c8699db7869d9a84e6b7c81626c9cf4
Jrf5x8 <jrf5x8@mail.missouri.edu>
2015-11-01 17:43:54 -0600

    testing a push of the CodeIgniter framework to GitHub

---
+++ application/models/LoginModel.php
@@ -0,0 +1,21 @@
+<?php
+
+    class LoginModel extends CI_Model{
+        public function login($username, $password){
+            $this->db->select('username', 'password');
+            $this->db->from('members');
+            $this->db->where('username', $username);
+            $this->db->where('password', $password);
+            
+            $query = $this->db->get();
+            
+            if($query->num_rows() == 1){
+                return true;
+            }else{
+                return false;
+            }
+        }
+    }
+
+
+?>
---
+++ application/views/home.php
@@ -0,0 +1,8 @@
+<!doctype html>
+<html>
+    <head><title></title></head>
+    <body>
+        
+        <h1>Welcome USer</h1>
+    </body>
+</html>
