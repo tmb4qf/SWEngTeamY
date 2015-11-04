9f35dd22970c17e2acf93d97f8c9aa5b7c0ecd1c
Jrf5x8 <jrf5x8@mail.missouri.edu>
2015-11-04 01:25:53 -0600

    deleted utility_helper.php...not needed

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -83,7 +83,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('form', 'url', 'utility');
+$autoload['helper'] = array('form', 'url');
 
 /*
 | -------------------------------------------------------------------
