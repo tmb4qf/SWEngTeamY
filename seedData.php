bc8dcab8e3f44931610419108a24737b13bfc39a
Tyler Brown <tmb4qf@mail.missouri.edu>
2015-11-03 22:09:49 -0600

    Add password hashing and more input fields

--- seedData.php
+++ seedData.php
@@ -1,13 +1,33 @@
 <!DOCTYPE html>
 <?php
+	require "password.php";
+	
 	$employeeID = $_POST['employeeID'];
 	$password = $_POST['pass'];
-	$userName = $_POST['uname'];
-	$title = $_POST['title'];
-	$academicOrg = $_POST['academicOrg'];
+	
+	$fname = $_POST['fname'];
+	$lname = $_POST['lname'];
 	$SSO = $_POST['sso'];
-	$address = $_POST['address'];
 	$phoneNum = $_POST['phone'];
+	$title = $_POST['title'];
+	$addrID = $_POST['addrID'];
+	
+	$street = $_POST['street'];
+	$city = $_POST['city'];
+	$state = $_POST['state'];
+	$country = $_POST['county'];
+	$zipCode = $_POST['zip'];
+	
+	$orgID = $_POST['orgID'];
+	$studentWorker = $_POST['studentWorker'];
+	
+	$jobTitle = $_POST['jobTitle'];
+	
+	$hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+	
+	//connection string to db
+	//query to add user info to db
 ?>
 <html>
 	<head>
@@ -15,19 +35,36 @@
 	</head>
 	<body>
 		<form action="seedData.php" method="POST">
-			<h1>Login Info</h1>
+			<h1>Authentication</h1>
 			EmployeeID: <input type="text" name="employeeID"><br>
 			Password: <input type="text" name="pass"><br>
 			<br/><br/>
-			<h1>Bio/Dem Info</h1>
-			User Name (Full legal name): <input type="text" name="uname"><br>
-			Title: <input type="text" name="title"><br>
-			Academic Organization: <input type="text" name="academicOrg"><br>
+			
+			<h1>Person</h1>
+			First Name: <input type="text" name="fname"><br>
+			Last Name: <input type="text" name="lname"><br>
 			PawPrint/SSO: <input type="text" name="sso"><br>
-			Campus Address: <input type="text" name="address"><br>
 			Phone-Number: <input type="text" name="phone"><br>
+			Title: <input type="text" name="title"><br>
+			Addr ID: <input type="text" name="addrID"><br>
 			<br/><br/>
+			
+			<h1>Address</h1>
+			Street: <input type="text" name="street"><br>
+			City: <input type="text" name="city"><br>
+			State: <input type="text" name="state"><br>
+			Country: <input type="text" name="country"><br>
+			Zip Code: <input type="text" name="zip"><br>
+			<br/><br/>
+			
+			<h1>Applicant</h1>
+			Organization ID: <input type="text" name="orgID"><br>
+			Is Student Worker <input type="text" name="studentWorker"><br>
+			
+			<h1>Application Processor</h1>
+			Job Title: <input type="text" name="jobTitle"><br>
 			<input type="submit" value="Submit">
+
 		</form>
 	</body>
 </html>
